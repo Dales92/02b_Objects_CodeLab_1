ca78e9f1eab3dbbd61d77dc7a9628de05422e5ba
Peter <schmidt.multimedia@gmail.com>
2022-04-11 12:50:21 +0200

    Konto() / Getter/Setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,21 @@ public class App {
     
     public static void main(String[] args) {
      
+        // Instanziierung
+        Konto konto1 = new Konto(10000);
+        Konto konto2 = new Konto(10000);
+        Konto konto3 = new Konto(10000);
+
+        // Abfrage Kontostand über Getter
+        output("Konto 1: " + konto1.getKontostand());
+        output("Konto 2: " + konto2.getKontostand());
+        output("Konto 3: " + konto3.getKontostand());
+        output("------------------");
+
+
+
+
+
     }
 
     public static void output(String outputStr) {
